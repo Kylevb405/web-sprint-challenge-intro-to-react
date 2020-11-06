4ea38a25ab16da955128cbb0a879991b27284c69
Kylevb405 <kylevanburen405@gmail.com>
2020-11-06 14:39:34 -0500

    finsihed project

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
+import Character from './components/Character'
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+
+  const [data, setData] = useState([])
+  
+const getData = useEffect(() => {
+  axios.get('https://swapi.dev/api/people/')
+  .then((res) => {
+
+    setData(res.data.results)
+
+  })
+  .catch(err => console.log(err));
+}, [])
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+
+
+      <Character CharacterData={data} />
+
+
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,55 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components'
+// Name, Gender, Height, Mass, BirthYear, Eye Color, Hair Color, Skin Color
+
+const Card = styled.div`
+
+    display:flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+    
+`
+
+const CharacterInfo = styled.div`
+
+    align-self: center;
+    margin-bottom: 20px;
+    width: 45%;
+    background-color : ${props => props.theme.primaryColor};
+
+`
+
+const Character = ({CharacterData}) => {
+    
+    return(
+
+        <Card>
+
+            {CharacterData.map((CharacterData) => {
+
+                return(
+
+                    <CharacterInfo>
+                        <h1>Name: {CharacterData.name}</h1>
+                        <h1>Gender: {CharacterData.gender}</h1>
+                        <h1>Height: {CharacterData.height}</h1>
+                        <h1>Mass: {CharacterData.mass}</h1>
+                        <h1>Birth Year: {CharacterData.birth_year}</h1>
+                        <h1>Eye Color: {CharacterData.eye_color}</h1>
+                        <h1>Hair Color: {CharacterData.hair_color}</h1>
+                        <h1>Skin Color: {CharacterData.skin_color}</h1>
+                    </CharacterInfo>
+
+                )
+
+            })}
+
+            
+        </Card>
+
+    )
+}
+
+export default Character
